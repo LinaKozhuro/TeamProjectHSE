f0210153a127b855b6a4c1dc06b82e79308c6a10
Полина Кожуро <pskozhuro@edu.hse.ru>
2018-06-09 17:21:41 +0300

    test2

---
+++ probnicteam2/Operators/Operator3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace probnicteam2
+{
+    public abstract class Operator3 : Function // third class for combinators
+    {
+        protected Operator3(Function q)
+        {
+            involVal = q;
+
+        }
+        protected readonly Function involVal;
+    }
+}
